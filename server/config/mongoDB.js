180e3a174193b8bfa1968da1ae7c659975b9ed9e
Dnyaneshwar2472 <maulijagadale9763@gmail.com>
2025-12-06 14:18:12 +0530

    update 1 file

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 async function  connectDB(){
     try {
-        const connction = await mongoose.connect('mongodb+srv://maulijagadale9763:nZeL8UQVHxoSEJ2C@thor.iw1otmk.mongodb.net/ecom');
-        console.log(`MongoDb connected at ${connction.connection.host}`)
+      
     } catch (error) {
         console.error(error);
         process.exit(1);
